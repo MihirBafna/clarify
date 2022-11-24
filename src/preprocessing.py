ad36c1a0d2702713b3b0ad4bde278555e1d31cf1
Mihir Bafna <mbafna6@gatech.edu>
2022-11-24 15:20:40 -0500

    fixed preprocessing issues

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -9,10 +9,13 @@ from rich.table import Table
 from rich.console import Console
 import random
 import sys
+from node2vec import Node2Vec
+
 sys.path.append('./submodules/CeSpGRN/src/')
 from submodules.CeSpGRN.src import *
 from submodules.CeSpGRN.src import kernel
 from submodules.CeSpGRN.src import g_admm as CeSpGRN
+
 
 
 def convert_adjacencylist2edgelist(adj_list): # do we need to account for repeat edges?
@@ -33,6 +36,19 @@ def convert_adjacencylist2edgelist(adj_list): # do we need to account for repeat
     return np.array(edge_list).T
 
 
+def convert_adjacencylist2adjacencymatrix(adj_list):
+    
+    num_vertices = len(adj_list)
+    adj_matrix = np.zeros(shape=(num_vertices,num_vertices))
+     
+    for i in range(num_vertices):
+        for j in adj_list[i]:
+            adj_matrix[i][j] = 1
+     
+    return adj_matrix
+    
+
+
 
 def select_LRgenes(data_df, num_genespercell, lr_database = 0):
     '''
@@ -42,7 +58,7 @@ def select_LRgenes(data_df, num_genespercell, lr_database = 0):
     :num_genespercell: int : represents the number of genes to be included for each Cell-Specific GRN
     :lr_database: int: 0/1/2 corresponding to LR database (0: CellTalkDB)
     
-    :return : pd.DataFrame with relevant gene columns preserved
+    :return : pd.DataFrame with relevant gene columns preserved and dictionary mapping LR genes to their numerical ids
     '''
     if lr_database == 0:
         sample_counts = data_df.drop(["Cell_ID", "X", "Y", "Cell_Type"], axis = 1)
@@ -59,17 +75,16 @@ def select_LRgenes(data_df, num_genespercell, lr_database = 0):
         
         print(f"Found {len(selected_ligands)} ligands and {len(selected_receptors)} receptors to be included in the {num_genespercell} selected genes per cell. \n")
         
-        
-        
         num_genesleft = num_genespercell - len(selected_ligands) - len(selected_receptors)
         candidate_genesleft = candidate_genes - selected_ligands - selected_receptors
         
         selected_randomgenes = set(random.sample(tuple(candidate_genesleft), num_genesleft))
         
         selected_genes = list(selected_randomgenes | selected_ligands | selected_receptors)
-        
+                
         selected_columns = ["Cell_ID", "X", "Y", "Cell_Type"] + np.vectorize(uppercase2real.get)(selected_genes).tolist()
         selected_df = data_df[selected_columns]
+
         
         console = Console()
         table = Table(show_header=True, header_style="bold")
@@ -78,8 +93,10 @@ def select_LRgenes(data_df, num_genespercell, lr_database = 0):
         table.add_column("Selected Random Genes", justify="right")
         table.add_row("\n".join(selected_ligands),"\n".join(selected_receptors),"\n".join(selected_randomgenes))
         console.print(table)
+                
+        lr2id = {gene:list(selected_df.columns).index(gene)-4 for gene in np.vectorize(uppercase2real.get)(list(selected_ligands|selected_receptors))}
         
-        return selected_df, selected_ligands|selected_receptors
+        return selected_df, lr2id
     else:
         raise Exception("Invalid lr_database type")
     
@@ -166,24 +183,13 @@ def construct_celllevel_graph(data_df, k, get_edges=False):   # Top k closest ne
                 edge_y.append(None)
         
     edges=[edge_x,edge_y]
-    
-        # for cell,adj in enumerate(tqdm(adjacency,desc=f"Getting the {k} edges of each cell", colour="cyan", position=1)):
-        #     x0, y0 = coordinate_df["X"].values[cell],coordinate_df["Y"].values[cell]
-        #     for ncell in adjacency[cell]:
-        #         x1, y1 = coordinate_df["X"].values[ncell],coordinate_df["Y"].values[ncell]
-        #         edge_x.append(x0)
-        #         edge_x.append(x1)
-        #         edge_x.append(None)
-        #         edge_y.append(y0)
-        #         edge_y.append(y1)
-        #         edge_y.append(None)
                 
     return adjacency,edges
 
 
 
 
-def construct_genelevel_graph(disjoint_grns, celllevel_adj_list, node_type = "int"):
+def construct_genelevel_graph(disjoint_grns, celllevel_adj_list, node_type = "int", lrgenes = None):
     '''
     Constructs gene level graph.
     
@@ -199,6 +205,7 @@ def construct_genelevel_graph(disjoint_grns, celllevel_adj_list, node_type = "in
     num2gene = {}   #dictionary that maps the supergraph integer nodes to the gene name (Cell#_gene#)
     gene2num = {}   #dictionary that maps the gene name (Cell#_gene#) to the supergraph integer node
 
+    assert max(lrgenes) <= numgenes
 
     grn_graph_list = []
     for cellnum, grn in enumerate(track(disjoint_grns, description=f"[cyan]3a. Combining individual GRNs")):
@@ -209,27 +216,66 @@ def construct_genelevel_graph(disjoint_grns, celllevel_adj_list, node_type = "in
             gene2num[f"Cell{cellnum}_Gene{i}"] = cellnum * numgenes + i
 
     union_of_grns = nx.disjoint_union_all(grn_graph_list)
-    
+        
     gene_level_graph = nx.relabel_nodes(union_of_grns, num2gene)  # relabel nodes to actual gene names
+    
+    # print(len(lrgenes), lrgenes)
 
+    # for cell, neighborhood in enumerate(track(celllevel_adj_list,\
+    #     description=f"[cyan]3b. Constructing Gene-Level Graph")): # for each cell in the ST data
+        
+    #     if lrgenes == None:     # randomize selection of genes to connect spatial edges
+    #         for genenum1 in range(numgenes):                            # for each gene in the cell                         45
+    #             node1 = f"Cell{cell}_Gene{genenum1}"
+    #             for ncell in neighborhood:                                  # for each neighborhood cell adjacent to cell   5
+    #                 candidate_neighbors=np.arange(numgenes,dtype=int)
+    #                 np.random.shuffle(candidate_neighbors)
+    #                 for genenum2 in candidate_neighbors[:numgenes//3]:          # for each gene in the neighborhood cell    45
+    #                     node2 = f"Cell{ncell}_Gene{genenum2}"
+    #                     gene_level_graph.add_edge(node1, node2)
+    #     else:
+    #         for genenum1 in lrgenes:                            # for each lr gene in the cell                         45
+    #             node1 = f"Cell{cell}_Gene{genenum1}"
+    #             for ncell in neighborhood:                                  # for each neighborhood cell adjacent to cell   5
+    #                 for genenum2 in lrgenes:                                    # for each gene in the neighborhood cell    45
+    #                     node2 = f"Cell{ncell}_Gene{genenum2}"
+    #                     gene_level_graph.add_edge(node1, node2)
+    
+        
     for cell, neighborhood in enumerate(track(celllevel_adj_list,\
         description=f"[cyan]3b. Constructing Gene-Level Graph")): # for each cell in the ST data
-        for genenum1 in range(numgenes):                            # for each gene in the cell                         45
-            node1 = f"Cell{cell}_Gene{genenum1}"
-            for ncell in neighborhood:                                  # for each neighborhood cell adjacent to cell   5
-                candidate_neighbors=np.arange(numgenes,dtype=int)
-                np.random.shuffle(candidate_neighbors)
-                for genenum2 in candidate_neighbors[:numgenes//3]:          # for each gene in the neighborhood cell    45
-                    node2 = f"Cell{ncell}_Gene{genenum2}"
+        for neighbor_cell in neighborhood:
+            for lrgene1 in lrgenes:
+                for lrgene2 in lrgenes:
+                    node1 = f"Cell{cell}_Gene{lrgene1}"
+                    node2 = f"Cell{neighbor_cell}_Gene{lrgene2}"
+                    if not gene_level_graph.has_node(node1) or not gene_level_graph.has_node(node2): 
+                        raise Exception("Node not found. Debug the Gene-Level Graph creation.")
+                    
                     gene_level_graph.add_edge(node1, node2)
 
-    
     if node_type == "str":
         gene_level_graph = gene_level_graph
     elif node_type == "int":
         gene_level_graph = nx.convert_node_labels_to_integers(gene_level_graph)
 
-    assert len(union_of_grns.nodes()) == numcells * numgenes
+    assert len(gene_level_graph.nodes()) == numcells * numgenes
 
     return gene_level_graph, num2gene, gene2num
         
+
+
+
+def get_gene_features(graph, type="node2vec"):
+    
+    console = Console()
+    console.print(f"[cyan]4. Constructing {type} Gene Level Features")
+    
+    if type == "node2vec":
+        node2vec = Node2Vec(graph, dimensions=64, walk_length=30, num_walks=200, workers=4)
+        model = node2vec.fit()
+
+        gene_feature_vectors = model.wv.vectors
+        
+    return gene_feature_vectors, model
+
